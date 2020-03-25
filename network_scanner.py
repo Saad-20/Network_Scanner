b876ecbd764c0a1a05860db74aa35b5fd2894306
Saad Shahzad <spider32277223@gmail.com>
2020-03-25 11:03:15 -0400

    Created IP & MAC table design

--- network_scanner.py
+++ network_scanner.py
@@ -7,13 +7,14 @@ def scan(ip):
     arp_request = scapy.ARP(pdst=ip)
     broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
     arp_request_broadcast = broadcast / arp_request
-    answered_list = scapy.srp(arp_request_broadcast, timeout=1)[0]
+    answered_list = scapy.srp(arp_request_broadcast, timeout=1, verbose=False)[0]
+
+    print("\tIP\t\t\t\t\t\t\tMAC Address\n---------------------------------------------")
 
     for element in answered_list:
-        print(element[1].psrc)
-        print(element[1].hwsrc)
-        print("---------------------------------------------------------------------------------------"
-              "-------------------------------")
+        # print ip address + print MAC address
+        print(element[1].psrc + "\t\t\t\t" + element[1].hwsrc)
+        print("---------------------------------------------")
     # print(answered_list.summary())
 
 
