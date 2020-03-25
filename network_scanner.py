225b7722619bef91399d952c9c9242ec4f1b7a59
Saad Shahzad <spider32277223@gmail.com>
2020-03-25 19:51:03 -0400

    Taking User Input and iterating result

--- network_scanner.py
+++ network_scanner.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
-
 import scapy.all as scapy
+from pip._vendor.distlib.compat import raw_input
 
 
 def scan(ip):
@@ -9,18 +9,28 @@ def scan(ip):
     arp_request_broadcast = broadcast / arp_request
     answered_list = scapy.srp(arp_request_broadcast, timeout=1, verbose=False)[0]
 
-    print("\tIP\t\t\t\t\t\t\tMAC Address\n---------------------------------------------")
-
+    clients_list = []
     for element in answered_list:
         # print ip address + print MAC address
-        print(element[1].psrc + "\t\t\t\t" + element[1].hwsrc)
-        print("---------------------------------------------")
-    # print(answered_list.summary())
+        clients_dict = {"ip": element[1].psrc, "mac": element[1].hwsrc}
+        clients_list.append(clients_dict)
+        return clients_list
+
+def print_result(results_list):
+    print("\tIP\t\t\t\tMAC Address\n---------------------------------------------------")
+    for client in results_list:
+        print(client)
+
+read_scan_ip = raw_input("[+] Enter ip address to scan: ")
+scan_result = scan(read_scan_ip)
+print_result(scan_result)
 
 
-scan("Put IP here")
 
 # arp_request.show()
 # broadcast.show()
 # arp_request_broadcast.show()
 # print(arp_request_broadcast.summary())
+# print(answered_list.summary())
+# print(element[1].psrc + "\t\t\t\t" + element[1].hwsrc)
+# print("---------------------------------------------")
