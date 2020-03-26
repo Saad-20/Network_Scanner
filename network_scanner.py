d6b41ffbd20ea2b1fbdb9d19d44fd6e3e2b4c6d3
Saad Shahzad <spider32277223@gmail.com>
2020-03-26 09:24:12 -0400

    Added comments

--- network_scanner.py
+++ network_scanner.py
@@ -2,20 +2,22 @@
 import scapy.all as scapy
 from pip._vendor.distlib.compat import raw_input
 
-
+# Defining the scan function
 def scan(ip):
     arp_request = scapy.ARP(pdst=ip)
     broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
     arp_request_broadcast = broadcast / arp_request
-    answered_list = scapy.srp(arp_request_broadcast, timeout=1, verbose=False)[0]
+    response_list = scapy.srp(arp_request_broadcast, timeout=1, verbose=False)[0]
 
+    # Creating list of clients in a dictonary and calling them out using the key of each dictionary via list
     clients_list = []
-    for element in answered_list:
+    for element in response_list:
         # print ip address + print MAC address
         clients_dict = {"ip": element[1].psrc, "mac": element[1].hwsrc}
         clients_list.append(clients_dict)
     return clients_list
 
+# Will return the results of each client stored in the client list
 def print_result(results_list):
     print("--------------------------------------------")
     print("|\tIP\t\t    MAC Address\t   |\n--------------------------------------------")
